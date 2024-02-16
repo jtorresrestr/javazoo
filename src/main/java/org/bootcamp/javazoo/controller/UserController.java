5ca8a92fbe08e33f17a2f581116a94e8de273ebe
Juan Toro <juan.toromejia@mercadolibre.com.co>
2024-02-16 15:35:18 -0500

    Request mapping added

--- src/main/java/org/bootcamp/javazoo/controller/UserController.java
+++ src/main/java/org/bootcamp/javazoo/controller/UserController.java
@@ -6,9 +6,11 @@ import org.bootcamp.javazoo.service.interfaces.IUserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final IUserService userService;
@@ -18,7 +20,7 @@ public class UserController {
         this.userService = userService;
         this.sellerService = sellerService;
     }
-    @GetMapping("/users/{userId}/followers/list")
+    @GetMapping("/{userId}/followers/list")
     public ResponseEntity<FollowersListDto> getFollowersList(@PathVariable Integer userId) {
         return ResponseEntity.ok(sellerService.getFollowersListService(userId));
     }
