ed3c491da9e7b3c5f017a02727e9570cee88cefd
dvilladiegop <danielarosa.villadiegopadilla@mercadolibre.com.co>
2024-02-19 00:32:46 -0500

    trabaje el punto US002, agregue el correspondiente ENDPOINT en el controller, agregue una nueva clase llamada CountFollowersDt con sus respectivos atributos.

--- src/main/java/org/bootcamp/javazoo/controller/UserController.java
+++ src/main/java/org/bootcamp/javazoo/controller/UserController.java
@@ -2,9 +2,11 @@ package org.bootcamp.javazoo.controller;
 
 import org.bootcamp.javazoo.dto.SellerDto;
 import org.bootcamp.javazoo.dto.UserDto;
+import org.bootcamp.javazoo.dto.response.CountFollowersDto;
 import org.bootcamp.javazoo.dto.response.FollowersListDto;
 import org.bootcamp.javazoo.service.interfaces.ISellerService;
 import org.bootcamp.javazoo.service.interfaces.IUserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,9 +32,17 @@ public class UserController {
     public ResponseEntity<FollowersListDto> getFollowersList(@PathVariable Integer userId) {
         return ResponseEntity.ok(sellerService.getFollowersListService(userId));
     }
-
     @GetMapping("/users/{userId}/followed/list")
     public ResponseEntity<?> getFollowedList(@PathVariable int userId){
         return ResponseEntity.ok(userService.getFollowedList(userId));
     }
+    // Obtener el resultado de la cantidad de usuarios que siguen a un determinado vendedor
+    @GetMapping("/{userId}/followers/count")
+    public ResponseEntity<CountFollowersDto> getFollowersCount(@PathVariable Integer userId){
+        Integer followersCount = userService.getFollowedList(userId).getFollowers().size();
+        String userName = userService.getFollowedList(userId).getUser_name();
+        CountFollowersDto countFollowersDto = new CountFollowersDto(userId, userName, followersCount);
+        return ResponseEntity.ok(countFollowersDto);
+    }
+
 }
